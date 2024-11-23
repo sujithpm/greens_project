a6f21782790c6b729c144cc4b4d041be7a445937
Sujith Murali <sujith.murali@outlook.com>
2024-11-23 10:00:56 +0530

    change issues in variabletf file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "project_name" { description = "The name of the project" type = string }
 
 variable "project_name"{
     description = "the name of the project"
     type = string
+    default = "sujithproj"
 }
 #this is the test line. added one line from local
